5bda44ce930fdbe562d9574270ca39c06ca0ba48
tuckkiez <i3adtzmaru@gmail.com>
2017-06-27 16:08:07 +0700

    change name scss

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 
 export default class Footer extends Component {
   render() {
-    const styles = require('./Footer.scss');
+    const styles = require('./footer.scss');
     const logoFooter = require('./full-logo-kingpower.png');
     return (
       <footer className={ styles.footer }>
