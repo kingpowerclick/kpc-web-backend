a551e2394d8127f6abaa8559f8a62c7ad453ce3b
tuckkiez <i3adtzmaru@gmail.com>
2017-07-05 17:39:13 +0700

    add product page

--- src/components/Product/ProductManageDynamicAttribute.js
+++ src/components/Product/ProductManageDynamicAttribute.js
@@ -5,7 +5,7 @@ export default class ProductManageDynamicAttribute extends Component {
   render() {
     const styles = require('./productFilters.scss');
     return (
-        <li className={ classNames( styles.filter, styles['filter-mda'])}><i className="fa fa-share-square-o"></i>Manage Dynamic Attribute</li>
+        <li className={ classNames( styles.filter, styles['filter-mda'])}><i className="fa fa-share-square-o"></i>Manage Custom Attribute</li>
     );
   }
 }
