25627e3505b2125282d1273d7dd095f0f22a8de6
Nuttaya Chutibuat <netty.ncz@gmail.com>
2017-07-18 15:11:50 +0700

    menu popup marketing

--- src/components/LeftMenu/LeftMenu.js
+++ src/components/LeftMenu/LeftMenu.js
@@ -15,6 +15,17 @@ export default class LeftMenu extends Component {
         document.getElementById('Products').classList.add('menu-product');
       }
     }
+    showSubmenuMarket = () => {
+      const marketing = document.getElementsByClassName('menu-marketing').length;
+      console.log('marketing : ' + marketing);
+      if (marketing > 0) {
+        document.getElementById('Marketing').classList.add('hidden');
+        document.getElementById('Marketing').classList.remove('menu-marketing');
+      } else {
+        document.getElementById('Marketing').classList.remove('hidden');
+        document.getElementById('Marketing').classList.add('menu-marketing');
+      }
+    }
     render() {
       const styles = require('./leftMenu.scss');
       const logoImg = require('./KP-white.png');
@@ -25,7 +36,7 @@ export default class LeftMenu extends Component {
                     <Link to={ `/orders` } className={ styles.menu }> <i className="fa fa-shopping-cart"></i></Link>
                 </OverlayTrigger>
                 <OverlayTrigger placement="right" overlay={<Tooltip id="menu-product">Products</Tooltip>}>
-                    <a className={ styles.menu } href="#" onClick={ this.showSubmenu } data-toggle="tooltip" title="Product">
+                    <a className={ styles.menu } href="#" onClick={ this.showSubmenu } >
                         <i className="fa fa-archive"></i>
                         <div className={ classNames(styles['sub-menu'], 'hidden') } id="Products">
                             <ul className="list-unstyled">
@@ -55,7 +66,23 @@ export default class LeftMenu extends Component {
                     <Link to={ `/customer` } className={ styles.menu }> <i className="fa fa-user"></i></Link>
                 </OverlayTrigger>
                 <OverlayTrigger placement="right" overlay={<Tooltip id="menu-marketing">Marketing</Tooltip>}>
-                    <a className={ styles.menu } href="#"> <i className="fa fa-bookmark"></i> </a>
+                    <a className={ styles.menu } href="#" onClick={ this.showSubmenuMarket }> <i className="fa fa-bookmark"></i>
+                        <div className={ classNames(styles['sub-menu'], 'hidden') } id="Marketing">
+                            <ul className="list-unstyled">
+                            <li className={ styles['sub-menu-head'] }>Marketing</li>
+                            <li className={ classNames('dropdown', styles['sub-menu-list']) }>
+                                <i className={ classNames('fa', 'fa-play', styles['sub-menu-list-hover']) }></i> Marketing
+                            </li>
+                            <li className={ classNames('dropdown', styles['sub-menu-list']) }>
+                                <i className={ classNames('fa', 'fa-play', styles['sub-menu-list-hover']) }></i> Specific Group
+                            </li>
+                            <li className={ classNames('dropdown', styles['sub-menu-list']) }>
+                                <i className={ classNames('fa', 'fa-play', styles['sub-menu-list-hover']) }></i> Brand and Member Discount Limitation
+                            </li>
+                            <li className={styles.footer}></li>
+                            </ul>
+                        </div>
+                    </a>
                 </OverlayTrigger>
                 <OverlayTrigger placement="right" overlay={<Tooltip id="menu-setting">Setting</Tooltip>}>
                     <a className={ classNames(styles.menu, 'hidden') } href="#"> <i className="fa fa-cog"></i> </a>
